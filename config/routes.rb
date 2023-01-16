fbf0ee9d1faf8bf9291a9da435873cce5a831a50
Nicolas Mariño <nicolasmarino@Nicolass-MacBook-Pro.local>
2023-01-16 18:42:40 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, path: ""
+  devise_for :users
   root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  devise_scope :user do
+   get 'users', to: 'devise/sessions#create'
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
