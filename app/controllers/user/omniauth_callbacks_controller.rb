6bed0f57cb3a4effb960f656ad062ac0b79aeca7
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2023-01-17 19:21:35 -0500

    add logic in omniauth; add secrets

--- app/controllers/user/omniauth_callbacks_controller.rb
+++ app/controllers/user/omniauth_callbacks_controller.rb
@@ -17,6 +17,15 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # end
   def google_oauth2
     user = User.create_authenticated_user(auth)
+
+    if user.present?
+      sign_out_all_scopes
+      flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect user, event: :authentication
+    else
+      flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} was not authenticated"
+      redirect_to new_user_session_path
+    end
   end
 
   # GET|POST /users/auth/twitter/callback
